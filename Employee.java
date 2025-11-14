b52019d6f18674d365f1e40fa2a397918d47cff9
Nikash <nikash238@gmail.com>
2025-11-14 10:33:06 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,17 @@
+// -------------------- Employee.java --------------------
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Name: " + name + " | Salary: " + salary;
+    }
+}
